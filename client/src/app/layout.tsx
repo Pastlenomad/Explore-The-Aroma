b5c8806f852b3913c21728fa45b1063184ed308f
Erban Danaj <danajerban@gmail.com>
2023-06-19 21:48:54 +0200

    FE refactor

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,17 +1,17 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Explore the Aroma',
-  description: 'Legacy project',
-}
+  title: "Explore the Aroma",
+  description: "Legacy project",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
